307700242e07052518379e62000abdb3b8ae78e8
markusstocker <markus.stocker@gmail.com>
2015-03-14 13:39:51 +0200

    first commit

---
+++ src/main/java/fi/uef/envi/emrooz/Emrooz.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.emrooz;
+
+/**
+ * <p>
+ * Title: Emrooz
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Emrooz
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2015
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class Emrooz {
+
+}
