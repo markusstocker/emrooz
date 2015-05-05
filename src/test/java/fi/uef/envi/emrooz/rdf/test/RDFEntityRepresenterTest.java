9272baebeb5bf271442e5da7b373d6097f7de781
markusstocker <markus.stocker@gmail.com>
2015-05-05 18:37:54 +0300

    moved to data-driven unit tests for RDFEntityRepresenter

--- src/test/java/fi/uef/envi/emrooz/rdf/test/RDFEntityRepresenterTest.java
+++ src/test/java/fi/uef/envi/emrooz/rdf/test/RDFEntityRepresenterTest.java
@@ -5,23 +5,21 @@
 
 package fi.uef.envi.emrooz.rdf.test;
 
-import java.util.HashSet;
 import java.util.Set;
 
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import junitparams.converters.ConvertParam;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
+import org.joda.time.DateTime;
 import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
-import org.openrdf.model.Value;
-import org.openrdf.model.ValueFactory;
-import org.openrdf.model.impl.ValueFactoryImpl;
-import org.openrdf.model.vocabulary.RDF;
-import org.openrdf.model.vocabulary.XMLSchema;
 
 import fi.uef.envi.emrooz.api.ssn.FeatureOfInterest;
 import fi.uef.envi.emrooz.api.ssn.ObservationValueDouble;
@@ -31,9 +29,7 @@ import fi.uef.envi.emrooz.api.ssn.SensorObservation;
 import fi.uef.envi.emrooz.api.ssn.SensorOutput;
 import fi.uef.envi.emrooz.api.time.Instant;
 import fi.uef.envi.emrooz.rdf.RDFEntityRepresenter;
-import fi.uef.envi.emrooz.vocabulary.DUL;
-import fi.uef.envi.emrooz.vocabulary.SSN;
-import fi.uef.envi.emrooz.vocabulary.Time;
+import fi.uef.envi.emrooz.test.ParamsConverterTest;
 
 /**
  * <p>
@@ -52,188 +48,80 @@ import fi.uef.envi.emrooz.vocabulary.Time;
  * @author Markus Stocker
  */
 
+@RunWith(JUnitParamsRunner.class)
 public class RDFEntityRepresenterTest {
 
 	private RDFEntityRepresenter representer;
-	private final static String ns = "http://example.org#";
-	private final static ValueFactory vf = ValueFactoryImpl.getInstance();
-	private final static DateTimeFormatter dtf = ISODateTimeFormat.dateTime()
-			.withOffsetParsed();
 
 	public RDFEntityRepresenterTest() {
 		representer = new RDFEntityRepresenter();
 	}
 
 	@Test
-	public void test1() {
-		Set<Statement> a = representer.createRepresentation(new Instant(vf
-				.createURI(ns + "t1"), dtf
-				.parseDateTime("2015-04-21T17:00:00.000+03:00")));
+	@FileParameters("src/test/resources/RDFEntityRepresenterTest-testInstant.csv")
+	public void testInstant(
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI id,
+			@ConvertParam(value = ParamsConverterTest.StringToDateTimeConverter.class) DateTime value,
+			@ConvertParam(value = ParamsConverterTest.StringToStatementsConverter.class) Set<Statement> expected,
+			String assertType) {
+		Set<Statement> actual = representer.createRepresentation(new Instant(
+				id, value));
 
-		Set<Statement> e = new HashSet<Statement>();
-		e.add(_statement(vf.createURI(ns + "t1"), RDF.TYPE, Time.Instant));
-		e.add(_statement(vf.createURI(ns + "t1"), Time.inXSDDateTime, vf
-				.createLiteral("2015-04-21T17:00:00.000+03:00",
-						XMLSchema.DATETIME)));
+		if (assertType.equals("assertEquals")) {
+			assertEquals(expected, actual);
+			return;
+		}
 
-		assertEquals(e, a);
+		assertNotEquals(expected, actual);
 	}
 
 	@Test
-	public void test2() {
-		Set<Statement> a = representer.createRepresentation(new Instant(vf
-				.createURI(ns + "t1"), dtf
-				.parseDateTime("2015-04-21T17:00:00.000+03:00")));
+	@FileParameters("src/test/resources/RDFEntityRepresenterTest-testObservationValueDouble.csv")
+	public void testObservationValueDouble(
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI id,
+			@ConvertParam(value = ParamsConverterTest.StringToDoubleConverter.class) Double value,
+			@ConvertParam(value = ParamsConverterTest.StringToStatementsConverter.class) Set<Statement> expected,
+			String assertType) {
+		Set<Statement> actual = representer
+				.createRepresentation(new ObservationValueDouble(id, value));
 
-		Set<Statement> e = new HashSet<Statement>();
-		e.add(_statement(vf.createURI(ns + "t1"), RDF.TYPE, Time.Instant));
-		e.add(_statement(vf.createURI(ns + "t1"), Time.inXSDDateTime, vf
-				.createLiteral("2015-04-21T17:01:00.000+03:00",
-						XMLSchema.DATETIME)));
+		if (assertType.equals("assertEquals")) {
+			assertEquals(expected, actual);
+			return;
+		}
 
-		assertNotEquals(e, a);
+		assertNotEquals(expected, actual);
 	}
 
 	@Test
-	public void test3() {
-		Set<Statement> a = representer
-				.createRepresentation(new ObservationValueDouble(vf
-						.createURI(ns + "ov1"), 0.0));
+	@FileParameters("src/test/resources/RDFEntityRepresenterTest-testSensorObservation.csv")
+	public void testSensorObservation(
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI id,
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI sensorId,
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI propertyId,
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI featureId,
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI outputId,
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI valueId,
+			@ConvertParam(value = ParamsConverterTest.StringToDoubleConverter.class) Double value,
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI timeId,
+			@ConvertParam(value = ParamsConverterTest.StringToDateTimeConverter.class) DateTime time,
+			@ConvertParam(value = ParamsConverterTest.StringToStatementsConverter.class) Set<Statement> expected,
+			String assertType) {
+		SensorObservation observation = new SensorObservation(id, new Sensor(
+				sensorId), new Property(propertyId), new FeatureOfInterest(
+				featureId));
+		observation.setObservationResult(new SensorOutput(outputId,
+				new ObservationValueDouble(valueId, value)));
+		observation.setObservationResultTime(new Instant(timeId, time));
 
-		Set<Statement> e = new HashSet<Statement>();
-		e.add(_statement(vf.createURI(ns + "ov1"), RDF.TYPE,
-				SSN.ObservationValue));
-		e.add(_statement(vf.createURI(ns + "ov1"), DUL.hasRegionDataValue,
-				vf.createLiteral(0.0)));
+		Set<Statement> actual = representer.createRepresentation(observation);
 
-		assertEquals(e, a);
-	}
+		if (assertType.equals("assertEquals")) {
+			assertEquals(expected, actual);
+			return;
+		}
 
-	@Test
-	public void test4() {
-		Set<Statement> a = representer
-				.createRepresentation(new SensorObservation(
-						vf.createURI(ns + "o1"),
-						new Sensor(vf.createURI(ns + "s1")),
-						new Property(vf.createURI(ns + "p1")),
-						new FeatureOfInterest(vf.createURI(ns + "f1")),
-						new SensorOutput(vf.createURI(ns + "so1"),
-								new ObservationValueDouble(vf.createURI(ns
-										+ "ov1"), 0.0)),
-						new Instant(vf.createURI(ns + "t1"), dtf
-								.parseDateTime("2015-04-21T18:00:00.000+03:00"))));
-
-		Set<Statement> e = new HashSet<Statement>();
-		e.add(_statement(vf.createURI(ns + "o1"), RDF.TYPE, SSN.Observation));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observedBy,
-				vf.createURI(ns + "s1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observedProperty,
-				vf.createURI(ns + "p1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.featureOfInterest,
-				vf.createURI(ns + "f1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observationResult,
-				vf.createURI(ns + "so1")));
-		e.add(_statement(vf.createURI(ns + "so1"), RDF.TYPE, SSN.SensorOutput));
-		e.add(_statement(vf.createURI(ns + "so1"), SSN.hasValue,
-				vf.createURI(ns + "ov1")));
-		e.add(_statement(vf.createURI(ns + "ov1"), RDF.TYPE,
-				SSN.ObservationValue));
-		e.add(_statement(vf.createURI(ns + "ov1"), DUL.hasRegionDataValue,
-				vf.createLiteral(0.0)));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observationResultTime,
-				vf.createURI(ns + "t1")));
-		e.add(_statement(vf.createURI(ns + "t1"), RDF.TYPE, Time.Instant));
-		e.add(_statement(vf.createURI(ns + "t1"), Time.inXSDDateTime, vf
-				.createLiteral("2015-04-21T18:00:00.000+03:00",
-						XMLSchema.DATETIME)));
-
-		assertEquals(e, a);
-	}
-	
-	@Test
-	public void test5() {
-		Set<Statement> a = representer
-				.createRepresentation(new SensorObservation(
-						vf.createURI(ns + "o1"),
-						new Sensor(vf.createURI(ns + "s1")),
-						new Property(vf.createURI(ns + "p1")),
-						new FeatureOfInterest(vf.createURI(ns + "f1")),
-						new SensorOutput(vf.createURI(ns + "so1"),
-								new ObservationValueDouble(vf.createURI(ns
-										+ "ov1"), 0.0)),
-						new Instant(vf.createURI(ns + "t1"), dtf
-								.parseDateTime("2015-04-21T18:00:00.000+03:00"))));
-
-		Set<Statement> e = new HashSet<Statement>();
-		e.add(_statement(vf.createURI(ns + "o1"), RDF.TYPE, SSN.Observation));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observedBy,
-				vf.createURI(ns + "s1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observedProperty,
-				vf.createURI(ns + "p1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.featureOfInterest,
-				vf.createURI(ns + "f1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observationResult,
-				vf.createURI(ns + "so1")));
-		e.add(_statement(vf.createURI(ns + "so1"), RDF.TYPE, SSN.SensorOutput));
-		e.add(_statement(vf.createURI(ns + "so1"), SSN.hasValue,
-				vf.createURI(ns + "ov1")));
-		e.add(_statement(vf.createURI(ns + "ov1"), RDF.TYPE,
-				SSN.ObservationValue));
-		e.add(_statement(vf.createURI(ns + "ov1"), DUL.hasRegionDataValue,
-				vf.createLiteral(0.1)));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observationResultTime,
-				vf.createURI(ns + "t1")));
-		e.add(_statement(vf.createURI(ns + "t1"), RDF.TYPE, Time.Instant));
-		e.add(_statement(vf.createURI(ns + "t1"), Time.inXSDDateTime, vf
-				.createLiteral("2015-04-21T18:00:00.000+03:00",
-						XMLSchema.DATETIME)));
-
-		assertNotEquals(e, a);
-	}
-	
-	@Test
-	public void test6() {
-		Set<Statement> a = representer
-				.createRepresentation(new SensorObservation(
-						vf.createURI(ns + "o1"),
-						new Sensor(vf.createURI(ns + "s1")),
-						new Property(vf.createURI(ns + "p1")),
-						new FeatureOfInterest(vf.createURI(ns + "f1")),
-						new SensorOutput(vf.createURI(ns + "so1"),
-								new ObservationValueDouble(vf.createURI(ns
-										+ "ov1"), 0.0)),
-						new Instant(vf.createURI(ns + "t1"), dtf
-								.parseDateTime("2015-04-21T18:00:00.000+03:00"))));
-
-		Set<Statement> e = new HashSet<Statement>();
-		e.add(_statement(vf.createURI(ns + "o1"), RDF.TYPE, SSN.Observation));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observedBy,
-				vf.createURI(ns + "s1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observedProperty,
-				vf.createURI(ns + "p1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.featureOfInterest,
-				vf.createURI(ns + "f1")));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observationResult,
-				vf.createURI(ns + "so1")));
-		e.add(_statement(vf.createURI(ns + "so1"), RDF.TYPE, SSN.SensorOutput));
-		e.add(_statement(vf.createURI(ns + "so1"), SSN.hasValue,
-				vf.createURI(ns + "ov1")));
-		e.add(_statement(vf.createURI(ns + "ov1"), RDF.TYPE,
-				SSN.ObservationValue));
-		e.add(_statement(vf.createURI(ns + "ov1"), DUL.hasRegionDataValue,
-				vf.createLiteral(0.0)));
-		e.add(_statement(vf.createURI(ns + "o1"), SSN.observationResultTime,
-				vf.createURI(ns + "t1")));
-		e.add(_statement(vf.createURI(ns + "t1"), RDF.TYPE, Time.Instant));
-		e.add(_statement(vf.createURI(ns + "t1"), Time.inXSDDateTime, vf
-				.createLiteral("2015-04-21T18:01:00.000+03:00",
-						XMLSchema.DATETIME)));
-
-		assertNotEquals(e, a);
-	}
-
-	private Statement _statement(URI s, URI p, Value o) {
-		return vf.createStatement(s, p, o);
+		assertNotEquals(expected, actual);
 	}
 
 }
--- src/test/java/fi/uef/envi/emrooz/test/ParamsConverterTest.java
+++ src/test/java/fi/uef/envi/emrooz/test/ParamsConverterTest.java
@@ -5,6 +5,12 @@
 
 package fi.uef.envi.emrooz.test;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
@@ -14,9 +20,13 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
+import org.openrdf.model.Value;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.vocabulary.XMLSchema;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
@@ -46,6 +56,8 @@ public class ParamsConverterTest {
 	private static final ValueFactory vf = ValueFactoryImpl.getInstance();
 	private static final DateTimeFormatter dtf = ISODateTimeFormat.dateTime()
 			.withOffsetParsed();
+	private static final Logger log = Logger
+			.getLogger(ParamsConverterTest.class.getName());
 
 	@Test
 	@Parameters({ "http://example.org#s1" })
@@ -111,6 +123,72 @@ public class ParamsConverterTest {
 		assertEquals(null, actual);
 	}
 
+	@Test
+	@Parameters({ "(http://example.org#s1 http://example.org#p1 http://example.org#o1);(http://example.org#s2 http://example.org#p2 http://example.org#o2)" })
+	public void testEqualsStringToStatements1(
+			@ConvertParam(value = StringToStatementsConverter.class) Set<Statement> actual) {
+		Set<Statement> expected = new HashSet<Statement>();
+		expected.add(vf.createStatement(vf.createURI("http://example.org#s1"),
+				vf.createURI("http://example.org#p1"),
+				vf.createURI("http://example.org#o1")));
+		expected.add(vf.createStatement(vf.createURI("http://example.org#s2"),
+				vf.createURI("http://example.org#p2"),
+				vf.createURI("http://example.org#o2")));
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	@Parameters({ "(http://example.org#s1 http://example.org#p1 \"0.0\"^^<http://www.w3.org/2001/XMLSchema#double>)" })
+	public void testEqualsStringToStatements2(
+			@ConvertParam(value = StringToStatementsConverter.class) Set<Statement> actual) {
+		Set<Statement> expected = new HashSet<Statement>();
+		expected.add(vf.createStatement(vf.createURI("http://example.org#s1"),
+				vf.createURI("http://example.org#p1"),
+				vf.createLiteral("0.0", XMLSchema.DOUBLE)));
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	@Parameters({ "(http://example.org#s1 http://example.org#p1 \"2015-04-21T17:01:00.000+03:00\"^^<http://www.w3.org/2001/XMLSchema#dateTime>)" })
+	public void testEqualsStringToStatements3(
+			@ConvertParam(value = StringToStatementsConverter.class) Set<Statement> actual) {
+		Set<Statement> expected = new HashSet<Statement>();
+		expected.add(vf.createStatement(vf.createURI("http://example.org#s1"),
+				vf.createURI("http://example.org#p1"), vf.createLiteral(
+						"2015-04-21T17:01:00.000+03:00", XMLSchema.DATETIME)));
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	@Parameters({ "(http://example.org#s1 http://example.org#p1 http://example.org#o1);(http://example.org#s2 http://example.org#p2 http://example.org#o2)" })
+	public void testNotEqualsStringToStatements1(
+			@ConvertParam(value = StringToStatementsConverter.class) Set<Statement> actual) {
+		Set<Statement> expected = new HashSet<Statement>();
+		expected.add(vf.createStatement(vf.createURI("http://example.org#s1"),
+				vf.createURI("http://example.org#p1"),
+				vf.createURI("http://example.org#o1")));
+		expected.add(vf.createStatement(vf.createURI("http://example.org#s3"),
+				vf.createURI("http://example.org#p2"),
+				vf.createURI("http://example.org#o2")));
+
+		assertNotEquals(expected, actual);
+	}
+
+	@Test
+	@Parameters({ "(http://example.org#s1 http://example.org#p1 \"2015-04-21T17:01:00.000+03:00\"^^<http://www.w3.org/2001/XMLSchema#dateTime>)" })
+	public void testNotEqualsStringToStatements3(
+			@ConvertParam(value = StringToStatementsConverter.class) Set<Statement> actual) {
+		Set<Statement> expected = new HashSet<Statement>();
+		expected.add(vf.createStatement(vf.createURI("http://example.org#s1"),
+				vf.createURI("http://example.org#p1"), vf.createLiteral(
+						"2015-04-21T17:00:00.000+03:00", XMLSchema.DATETIME)));
+
+		assertNotEquals(expected, actual);
+	}
+
 	public static class StringToURIConverter implements ParamConverter<URI> {
 		public URI convert(Object param, String options) {
 			String value = param.toString();
@@ -133,7 +211,6 @@ public class ParamsConverterTest {
 
 			return Double.valueOf(value);
 		}
-
 	}
 
 	public static class StringToDateTimeConverter implements
@@ -146,7 +223,81 @@ public class ParamsConverterTest {
 
 			return dtf.parseDateTime(value);
 		}
+	}
 
+	public static class StringToStatementsConverter implements
+			ParamConverter<Set<Statement>> {
+		public Set<Statement> convert(Object param, String options) {
+			String value = param.toString();
+
+			if (value.isEmpty())
+				return Collections.emptySet();
+
+			Set<Statement> ret = new HashSet<Statement>();
+
+			String label;
+			String datatype;
+			String[] statements = value.split(";");
+
+			for (String statement : statements) {
+				if (statement.startsWith("(") && statement.endsWith(")")) {
+					statement = statement.substring(1, statement.length() - 1);
+				} else {
+					if (log.isLoggable(Level.WARNING))
+						log.warning("Statement should start and end with rounded parenthesis [statement = "
+								+ statement
+								+ "; statements = "
+								+ statements
+								+ "]");
+					continue;
+				}
+
+				String[] resources = statement.split(" ");
+				int length = resources.length;
+
+				if (length != 3) {
+					if (log.isLoggable(Level.WARNING))
+						log.warning("Expected three resources [resources.length = "
+								+ length
+								+ "; statement = "
+								+ statement
+								+ "; statements = " + statements + "]");
+					continue;
+				}
+
+				Resource s = vf.createURI(resources[0]);
+				URI p = vf.createURI(resources[1]);
+
+				Value o = null;
+				String v = resources[2];
+
+				if (v.contains("^^")) {
+					label = v.substring(v.indexOf("\"") + 1,
+							v.lastIndexOf("\""));
+					datatype = v.substring(v.indexOf("<") + 1,
+							v.lastIndexOf(">"));
+
+					if (datatype.equals(XMLSchema.DATETIME.stringValue())) {
+						o = vf.createLiteral(label, XMLSchema.DATETIME);
+					} else if (datatype.equals(XMLSchema.DOUBLE.stringValue())) {
+						o = vf.createLiteral(label, XMLSchema.DOUBLE);
+					} else {
+						if (log.isLoggable(Level.WARNING)) {
+							log.warning("Unrecognized datatype [datatype = "
+									+ datatype + "; statement = " + statement
+									+ "; statements = " + statements + "]");
+							continue;
+						}
+					}
+				} else {
+					o = vf.createURI(v);
+				}
+
+				ret.add(vf.createStatement(s, p, o));
+			}
+
+			return Collections.unmodifiableSet(ret);
+		}
 	}
 
 }
