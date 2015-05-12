82ed2bbe2e88830047b489202a9a9053fe472d43
markusstocker <markus.stocker@gmail.com>
2015-05-12 20:31:57 +0300

    start thinking about registering sensors and frequency measurement capability in a knowledge store, separate from the data store; only placeholders so far

---
+++ src/main/java/fi/uef/envi/emrooz/store/DataStore.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.emrooz.store;
+
+/**
+ * <p>
+ * Title: DataStore
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Emrooz
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2015
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class DataStore {
+
+}
---
+++ src/main/java/fi/uef/envi/emrooz/store/KnowledgeStore.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.emrooz.store;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+
+/**
+ * <p>
+ * Title: KnowledgeStore
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Emrooz
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2015
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class KnowledgeStore {
+
+	private Repository repository;
+	private RepositoryConnection connection;
+	
+	private static final Logger log = Logger.getLogger(KnowledgeStore.class.getName());
+	
+	public KnowledgeStore(Repository repository) {
+		if (repository == null)
+			throw new RuntimeException("[repository = null]");
+		
+		this.repository = repository;
+		
+		try {
+			this.repository.initialize();
+			this.connection = this.repository.getConnection();
+		} catch (RepositoryException e) {
+			if (log.isLoggable(Level.SEVERE))
+				log.severe(e.getMessage());
+		}
+	}
+	
+}
