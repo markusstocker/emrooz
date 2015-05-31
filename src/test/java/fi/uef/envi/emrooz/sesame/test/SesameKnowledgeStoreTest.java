977ab5f8bd1d9e1dbd8c046be28f5511896587f0
markusstocker <markus.stocker@gmail.com>
2015-05-31 18:05:08 +0300

    added SesameKnowledgeStore#addSensor tests

--- src/test/java/fi/uef/envi/emrooz/sesame/test/SesameKnowledgeStoreTest.java
+++ src/test/java/fi/uef/envi/emrooz/sesame/test/SesameKnowledgeStoreTest.java
@@ -20,11 +20,14 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.rio.RDFParseException;
 import org.openrdf.sail.memory.MemoryStore;
 
+import fi.uef.envi.emrooz.entity.ssn.FeatureOfInterest;
+import fi.uef.envi.emrooz.entity.ssn.Property;
 import fi.uef.envi.emrooz.entity.ssn.Sensor;
 import fi.uef.envi.emrooz.rdf.RDFEntityRepresenter;
 import fi.uef.envi.emrooz.sesame.SesameKnowledgeStore;
@@ -61,8 +64,8 @@ public class SesameKnowledgeStoreTest {
 		ks.load(new File(kb));
 		
 		RDFEntityRepresenter er = new RDFEntityRepresenter();
+		
 		Set<Sensor> e = er.createSensors(statements);
-
 		Set<Sensor> a = ks.getSensors();
 
 		if (assertType.equals("assertEquals")) {
@@ -74,4 +77,32 @@ public class SesameKnowledgeStoreTest {
 		
 		ks.close();
 	}
+	
+	@Test
+	@FileParameters("src/test/resources/SesameKnowledgeStoreTest-testAddSensor.csv")
+	public void testAddSensor(
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI sensorId,
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI propertyId,
+			@ConvertParam(value = ParamsConverterTest.StringToURIConverter.class) URI featureId,
+			@ConvertParam(value = ParamsConverterTest.StringToStatementsConverter.class) Set<Statement> statements,
+			String assertType) throws RepositoryException, RDFParseException,
+			IOException {
+		SesameKnowledgeStore ks = new SesameKnowledgeStore(new SailRepository(new MemoryStore()));
+		ks.addSensor(new Sensor(sensorId, new Property(propertyId, new FeatureOfInterest(featureId))));
+		
+		RDFEntityRepresenter er = new RDFEntityRepresenter();
+		
+		Set<Sensor> e = er.createSensors(statements);
+		Set<Sensor> a = ks.getSensors();
+		
+		if (assertType.equals("assertEquals")) {
+			assertTrue(CollectionUtils.isEqualCollection(e, a));
+			return;
+		}
+
+		assertFalse(CollectionUtils.isEqualCollection(e, a));
+		
+		ks.close();
+	}
+
 }
