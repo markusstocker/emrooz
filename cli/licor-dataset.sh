2f758569741fd02ee1998748cc1aded73c985d6b
markusstocker <markus.stocker@gmail.com>
2015-07-25 17:19:08 +0300

    split licor.sh into one for sensor and one for dataset observations

---
+++ cli/licor-dataset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CLASSPATH="../lib/*"
+
+if [ -n "${JAVA_HOME}" -a -x "${JAVA_HOME}/bin/java" ]; then
+ java="${JAVA_HOME}/bin/java"
+else
+ java=java
+fi
+
+exec "${java}" -client -classpath "${CLASSPATH}" -Djava.util.logging.config.file=logging.properties fi.uef.envi.emrooz.io.licor.GHGDatasetObservationReader "$@"
