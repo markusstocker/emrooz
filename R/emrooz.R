d1427a613995eb41d0d3b1d1b17e752da3eec59d
markusstocker <markus.stocker@gmail.com>
2015-08-02 19:54:18 +0300

    set host to variable

--- R/emrooz.R
+++ R/emrooz.R
@@ -2,13 +2,15 @@ library(RCurl)
 library(ggplot2)
 library(grid)
 
+host <- "http://localhost:8080"
+
 # Print with fractional seconds
 op <- options(digits.secs = 3) 
 
-df.sensors <- read.csv(text=getURL("http://localhost:8080/sensors/list"), header=FALSE, col.names=c("sensor"))
-df.properties <- read.csv(text=getURL("http://localhost:8080/properties/list"), header=FALSE, col.names=c("property"))
-df.features <- read.csv(text=getURL("http://localhost:8080/features/list"), header=FALSE, col.names=c("feature"))
-df.datasets <- read.csv(text=getURL("http://localhost:8080/datasets/list"), header=FALSE, col.names=c("dataset"))
+df.sensors <- read.csv(text=getURL(paste0(host, "/sensors/list")), header=FALSE, col.names=c("sensor"))
+df.properties <- read.csv(text=getURL(paste0(host, "/properties/list")), header=FALSE, col.names=c("property"))
+df.features <- read.csv(text=getURL(paste0(host, "/features/list")), header=FALSE, col.names=c("feature"))
+df.datasets <- read.csv(text=getURL(paste0(host, "/datasets/list")), header=FALSE, col.names=c("dataset"))
 
 sensor <- "http://example.org#aThermometer"
 property <- "http://example.org#temperature"
@@ -24,7 +26,7 @@ from <- "2015-01-07T00:15:00.000+06:00"
 to <- "2015-01-07T00:20:00.000+06:00"
 ylab <- "CO2 [mmol m-3]"
 
-url <- paste0("http://localhost:8080/observations/sensor/list?", "sensor=", curlEscape(sensor), "&property=", curlEscape(property), "&feature=", curlEscape(feature), "&from=", curlEscape(from), "&to=", curlEscape(to))
+url <- paste0(host, "/observations/sensor/list?", "sensor=", curlEscape(sensor), "&property=", curlEscape(property), "&feature=", curlEscape(feature), "&from=", curlEscape(from), "&to=", curlEscape(to))
 df.observations <- read.csv(text=getURL(url, httpheader=c(Accept="text/csv")), header=TRUE, sep=",")
 # Correction because strptime %z expects +0300 while ISO is +03:00
 df.observations$time <- strptime(gsub("([+-]\\d\\d)(:)", "\\1", df.observations$time), "%Y-%m-%dT%H:%M:%OS%z", tz="UTC") # Canonicalize to UTC
@@ -33,7 +35,7 @@ ggplot(data=df.observations, aes(time, value)) + geom_line() + xlab("Time") + yl
 dataset <- "http://example.org#d1"
 from <- "2015-01-07T00:15:00.000+06:00"
 to <- "2015-01-07T00:20:00.000+06:00"
-url <- paste0("http://localhost:8080/observations/dataset/list?", "dataset=", curlEscape(dataset), "&from=", curlEscape(from), "&to=", curlEscape(to))
+url <- paste0(host, "/observations/dataset/list?", "dataset=", curlEscape(dataset), "&from=", curlEscape(from), "&to=", curlEscape(to))
 df.observations <- read.csv(text=getURL(url, httpheader=c(Accept="text/csv")), header=TRUE, sep=",")
 df.observations$time <- strptime(gsub("([+-]\\d\\d)(:)", "\\1", df.observations$time), "%Y-%m-%dT%H:%M:%OS%z", tz="UTC") # Canonicalize to UTC
 plot1 <- df.observations %>%
